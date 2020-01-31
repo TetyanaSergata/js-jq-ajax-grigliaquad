40d9080ddccd70ed0d10a6b6cff1da66516eb592
Tetyana <tsergata@gmail.com>
2020-01-31 14:29:41 +0100

    Correzzioni

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 // Griglia 6x6, ad ogni click parte una richiesta AJAX che prende un numero random da 1 a 9.
 // Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde. Il numero ottenuto appare al centro del quadrato.
 $(document).ready(function() {
-  $('.grid').click(function() {
+
+  // Generiamo 36 quadrati con un ciclo
+  for (var i = 0; i < 35; i++) {
+    var grid = $('.template .grid').clone();
+    $('.grid-container').append(grid);
+  }
+
+  // Evento click
+  $(document).on('click', '.grid', function() {
     var element = $(this);
 
     // Chiamata AJAX
@@ -12,12 +20,14 @@ $(document).ready(function() {
         'success': function (response) {
           console.log(response.response);
           console.log(this);
+
           var number = response.response;
           if (number <= 5) {
             element.addClass('green');
           }else{
             element.addClass('yellow');
           }
+          element.text(number);
         },
         'error': function (richiesta, stato, errori) {
           alert("E' avvenuto un errore. " + errore);
